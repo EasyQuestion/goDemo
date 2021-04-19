3e689edbdd1b20269e64cfd05ca4ecfe3672401f
mumh <minghui.mu@dadaabc.net>
2021-04-19 16:54:41 +0800

    add hanshunping chapter4_4

--- main/hanshunpingGo/chapter4/main/6struct.go
+++ main/hanshunpingGo/chapter4/main/6struct.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 /*type Cat struct {
 	Name  string
@@ -41,8 +44,8 @@ func main() {
 	fmt.Println("monster1=", monster1)
 	fmt.Println("monster2=", monster2)
 }*/
-
-type Person struct {
+//---------------------------------------------
+/*type Person struct {
 	Name string
 	Age int
 }
@@ -54,6 +57,71 @@ func main() {
 	fmt.Println("p1=",p1)
 	fmt.Println("p2=",p2)
 	//fmt.Println(*p2.Name) // 编译出错，不能这样写
+}*/
+//-----------------------------------------
+/*type Point struct {
+	x int
+	y int
+}
+
+type React struct {
+	leftUp, rightDown Point
+}
+
+type React2 struct {
+	leftUp, rightDown *Point
+}
+
+func main() {
+	r1 := React{Point{1, 2}, Point{3, 4}}
+	// 结构体的所有字段在内存中是连续的
+	// 这样取值时会非常快，效率高
+	fmt.Printf("r1.leftUp.x的地址%p,r1.leftUp.y的地址%p,r1.rightDown.x的地址%p,r1.rightDown.y的地址%p \n",
+		&r1.leftUp.x, &r1.leftUp.y, &r1.rightDown.x, &r1.rightDown.y)
+
+	r2 := React2{&Point{10, 20}, &Point{30, 40}}
+	// 当struct中的内容是指针时，指针所分配的地址仍然是连续的，但是指针的内容有可能不是连续的...
+	fmt.Printf("r2.leftUp的地址%p,r1.rightDown的地址%p,\n", &r2.leftUp, &r2.rightDown)
+	fmt.Printf("r2.leftUp的值%p,r1.rightDown的值%p,\n", r2.leftUp, r2.rightDown)
+}*/
+//--------------------------------------------------------------------------
+/*type A struct {
+	Num int
+}
+
+type B struct {
+	Num int
+	//Color string  不论是A多了Color字段，还是B多个Color字段，AB的强制转换都会编译不通过
+}
+
+//type C B // 此时编译器认为B,C是2种不同的数据类型，只能强制类型转换
+type C = B // 可以这样，相当于起别名
+
+func main() {
+	var a A
+	var b B
+	a = A(b) // 可以把B类型强制转换成A类型
+	fmt.Println(a, b)
+	var c C
+	c = b
+	fmt.Println(c)
+}*/
+//--------------------------------------------------------------------------
+type Monster struct {
+	Name   string `json:"name"` // 通过tag标签来实现json的序列化
+	Age    int `json:"age"`
+	Skills string `json:"skills"`
+}
+
+func main() {
+	//序列化json和反序列化
+	monster := Monster{"铁扇公主", 500, "芭蕉扇"}
+	marshal, err := json.Marshal(monster)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("序列化成json的值为%s \n", marshal)
+	}
 }
 
 // go 通过结构体struct来实现面向对象编程的特性
@@ -63,7 +131,10 @@ func main() {
 // struct 是值类型，变量直接指向具体的值
 // 定义struct的4种方式：
 // 1.先声明类型，再每个属性赋值
-// 2.monster1 := Monster{Name: "牛魔王", Age: 500}
+// 2.monster1 := Monster{  这样就不用和定义字段的顺序保持一致了
+//         Name: "牛魔王",
+//         Age: 500,
+//        }
 //   monster1 := Monster{"牛魔王", 500}
 //   monster1 := Monster{}
 // 3.var person *Person = new(Person) 返回一个指针类型
@@ -74,3 +145,6 @@ func main() {
 
 // struct使用注意事项
 // 1.结构体的所有字段在内存中是连续的
+// 2.结构体是用户自定义的类型，和其它类型进行转换时需要有 完全相同的字段（名称、个数、类型）
+// 3.结构体进行type重新定义后，编译器认为是2个不同的类型，但是可以相互强制转换
+// 4.struct每个字段上，可以写上一个tag,该tag可以通过反射获取到，常见的使用场景是序列化和反序列化
---
+++ main/hanshunpingGo/chapter4/main/7method.go
@@ -0,0 +1,139 @@
+package main
+
+import "fmt"
+
+/*type Person struct {
+	Name string
+}
+
+//func (p Person) test() {
+func (p *Person) test() {
+	p.Name = "jack"
+	fmt.Println("test() ", p.Name)
+}
+
+func main() {
+	p := Person{}
+	p.Name = "tom"
+	p.test()
+	fmt.Println("main() ", p.Name)
+}*/
+/*type integer int
+func (i integer) print() {
+	fmt.Println(i)
+}
+
+func main() {
+	var i integer = 10
+	i.print()
+}*/
+//-------------------------------------------------
+/*type Student struct {
+	Name string
+	Age  int
+}
+
+func (stu *Student) String() string {
+	stu.Name = "tom"
+	fmt.Println("get String()...")
+	return fmt.Sprintf("Name=%v Age=%v", stu.Name, stu.Age)
+}
+
+func main() {
+	stu := Student{"jack", 12}
+	fmt.Println(&stu)
+}*/
+//------------------------------------------------
+/*type MethodUtils struct {
+	// 结构体内可以没有任何字段
+}
+
+func (md MethodUtils) Print() {
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 8; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
+func (md MethodUtils) Print2(m, n int) {
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
+func (md MethodUtils) area(length, width float64) float64 {
+	return length * width
+}
+
+// 编写一个结构体MethodUtils ,给它编写一个方法,返回10*8的矩形,并在main函数中调用
+func main() {
+	mu := MethodUtils{}
+	mu.Print()
+	fmt.Println()
+	mu.Print2(2, 6)
+	fmt.Println()
+	fmt.Println("area=", mu.area(2.5, 2))
+}*/
+//----------------------------------------------------
+/*func main() {
+	data := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	fmt.Println(data)
+	for i := 0; i < len(data); i++ {
+		for j := i + 1; j < len(data[i]); j++ {
+			data[i][j], data[j][i] = data[j][i], data[i][j]
+		}
+	}
+	fmt.Println()
+	fmt.Println(data)
+}*/
+//----------------------------------------------------
+type Person struct {
+	Name string
+	Age int
+}
+func (p Person) Print() {
+	p.Name = "jack"
+	fmt.Println("print() ",p.Name)
+}
+func (p *Person) Print2() {
+	p.Name = "mary"
+	fmt.Println("print() ",p.Name)
+}
+
+func main() {
+	// 方法比函数多了一个变量类型,这个变量类型有优化,指针和变量可以用简写的方式来调用
+	p := Person{"tom",12}
+	p.Print()
+	fmt.Println("main() ",p.Name)
+	(&p).Print()
+	fmt.Println("main() ",p.Name)
+
+	p.Print2()
+	fmt.Println("main() ",p.Name)
+	(&p).Print2()
+	fmt.Println("main() ",p.Name)
+}
+
+// go中的方法：作用在指定的数据类型上(是和数据类型绑定的)
+// 自定义的类型，都可以有方法，不仅仅是struct
+
+// 方法的调用和传参机制和函数基本一样，唯一多的是绑定了数据类型，会把调用方法的变量也当作实参传递给方法
+// 如果调用方法的变量是值类型，会进行值拷贝，如果是引用类型，则会进行引用传递
+// 所以在方法上的常用情况 func (p *Person) test(){...
+
+// 方法使用的注意事项
+// 1.结构体是值类型,在方法调用时,会进行值拷贝
+// 2.如果希望在方法体修改结构体的值,可以通过结构体指针的方式来处理
+// 3.golang中的方法作用在具体的数据类型上,和指定的数据类型绑定,
+//            因此自定义类型,都可以有方法,不仅仅是struct,还有int,float32等待
+// 4.方法的访问范围控制规则,和函数一样,首字母大写,可以在本包和其他包访问;首字母小写,只能在本包内访问
+// 5.如果一个类型实现了String()方法,那么fmt.Println()默认会调用变量的String()方法进行输出
+//
+// 方法与函数的区别
+// 1.调用方式不同:方法:数据类型对应的变量.方法(),函数:包名.函数名()
+// 2.方法比函数多了一个变量类型,这个变量类型有优化,指针和变量可以简写的方式来调用
+
+// 结构体创建字段时可以直接指定值
